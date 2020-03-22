e7dbba2bf9e6310f08edebe37b36eed3120f9da1
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 02:24:45 +0300

    Initial commit

---
+++ Kod S Faktoriyel Hesapla v2.0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Faktoriyel_Hesapla_v2._0
+{
+    class Program
+    {
+        static int fak(int x)
+        {
+            int carpim = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                carpim *= i;
+            }
+            return carpim;
+        }
+
+        static void Main(string[] args)
+        {
+            int x, z;
+            Console.Write("Sayı Giriniz:");
+            x = int.Parse(Console.ReadLine());
+            z = fak(x);
+            Console.WriteLine();
+            Console.Write("Sonuç:");
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_11_Faktoriyel_Hesapla_v.2.0
+ 
